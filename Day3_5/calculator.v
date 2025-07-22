2045865dd12987312bba0a9223cfd45b84658005
uglyd0cky <eaterofworms@outlook.com>
2025-07-21 21:25:43 -0300

    Create calculator.v

---
+++ Day3_5/calculator.v
@@ -0,0 +1,38 @@
+\m4_TLV_version 1d: tl-x.org
+\SV
+   // This code can be found in: https://github.com/stevehoover/RISC-V_MYTH_Workshop
+   
+   m4_include_lib(['https://raw.githubusercontent.com/stevehoover/RISC-V_MYTH_Workshop/ecba3769fff373ef6b8f66b3347e8940c859792d/tlv_lib/calculator_shell_lib.tlv'])
+
+\SV
+   m4_makerchip_module   // (Expanded in Nav-TLV pane.)
+
+\TLV
+   |calc
+      @0
+         $reset = *reset;
+         
+         $out[31:0] = ($op[1:0] == 2'b00) ? $val1[31:0] + $val2[31:0] : ($op[1:0] == 2'b01) ? $val1[31:0] - $val2[31:0] : ($op[1:0] == 2'b10) ? $val1[31:0] * $val2[31:0] : ($op[1:0] == 2'b11) ? $val1[31:0] / $val2[31:0] : 31'b0;
+         $val1[31:0] = $rand1[3:0];
+         $val2[31:0] = $rand2[3:0];
+         
+         
+
+      // Macro instantiations for calculator visualization(disabled by default).
+      // Uncomment to enable visualisation, and also,
+      // NOTE: If visualization is enabled, $op must be defined to the proper width using the expression below.
+      //       (Any signals other than $rand1, $rand2 that are not explicitly assigned will result in strange errors.)
+      //       You can, however, safely use these specific random signals as described in the videos:
+      
+      
+      
+   //m4+cal_viz(@3) // Arg: Pipeline stage represented by viz, should be atleast equal to last stage of CALCULATOR logic.
+
+   
+   // Assert these to end simulation (before Makerchip cycle limit).
+   *passed = *cyc_cnt > 40;
+   *failed = 1'b0;
+   
+
+\SV
+   endmodule
