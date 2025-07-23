73deff44db4714dac4b3bd633fc6f08145b656dc
Maria Luiza <56833489+malu-alves00@users.noreply.github.com>
2025-07-23 23:08:01 +0530

    Add files via upload

---
+++ Day1_2/signedNegative.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	long long int max = (long long int) (pow(2,10) * -1);
+	printf("Highest number in long long int: %lli\n", max);
+	return 0;
+}
